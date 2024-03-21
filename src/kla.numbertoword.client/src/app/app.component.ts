28bc1027af396402d714aafcc325ec6df9f48bcf
Peyman Ebrahimi <peyman.ebrahimi@gmail.com>
2024-03-21 23:20:14 +0330

    add style

--- src/kla.numbertoword.client/src/app/app.component.ts
+++ src/kla.numbertoword.client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,13 +7,11 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
-import { Observable, catchError, config, of, tap, throwError } from 'rxjs';
+import { Observable, catchError, of, tap } from 'rxjs';
 import { ConversionResponse, WordToNumberService } from './word-to-number.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
-
-import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -47,13 +46,12 @@ export class AppComponent {
   })
 
   convertToWord() {
-    var x =this.frm.controls.userInput.value!.toString();
-    this.response$ = this.wordToNumberService.getConvert(this.frm.controls.userInput.value!.toString())
+    var data = this.frm.controls.userInput.value!.toString();
+    this.response$ = this.wordToNumberService.getConvert(data)
       .pipe(
-        tap(()=>console.log(`sent: ${x}`)),
+        tap(() => console.log(`sent: ${data}`)),
         catchError(this.handleError.bind(this))
       );
-    //console.info(this.frm.controls.userInput.value);
   }
 
   // this should not be here, but the task is for c# developer ;)
@@ -67,8 +65,8 @@ export class AppComponent {
         duration: 5000
       });
     }
-    // Return an observable with a user-facing error message.
-    return of();// throwError(() => new Error('Something bad happened; please try again later.'));
+
+    return of();
   }
 }
 
